f3d2ae2c247f6009d66ca279f422725f86e44ff5
hari ram <hariram@haris-MacBook-Pro.local>
2023-08-09 15:28:36 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * @format
+ */
+
+import { AppRegistry } from 'react-native';
+import App from './App';
+import { name as appName } from './app.json';
+import HeadlessTask from './src/utils/HeadlessTask';
+
+AppRegistry.registerHeadlessTask('RNFirebaseBackgroundNotificationAction', HeadlessTask);
+AppRegistry.registerHeadlessTask('RNFirebaseBackgroundMessage', () => HeadlessTask);
+
+AppRegistry.registerComponent(appName, () => App);
